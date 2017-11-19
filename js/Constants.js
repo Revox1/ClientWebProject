842f65753d25af84356e38dfde8ca6c6feb42d18
Mutescu Alexandru Ciprian <revox1@users.noreply.github.com>
2017-11-19 16:57:26 +0200

    Rewriting popover to be added as shadow dom element

--- js/Constants.js
+++ js/Constants.js
@@ -1,5 +1,6 @@
 const constants = {
     popoverMainClass:"container",
     popoverID:"unique_popover",
+    popoverID2:"popoverID",
     modal_button:"open_modal"
 };
--- js/content.js
+++ js/content.js
@@ -17,37 +17,49 @@ function httpGetAsynca(theUrl, callback) {
 window.onload = function () {
     var current_popover;
 
-
     httpGetAsynca("html/extension_popover.html", function (data) {
 
         var inject = document.createElement("div");
-        inject.innerHTML = data;
+        inject.id = constants.popoverID;
+        // inject.innerHTML = data;
         document.body.insertBefore(inject, document.body.firstChild);
 
-        window.addEventListener("mouseover", function (event) {
-            if (event.target.tagName === "IMG" && !findAncestor(event.target, constants.popoverMainClass)) {
-                var popover = document.getElementById(constants.popoverID);
-                popover.style.top = event.target.offsetTop.toString() + "px";
-                popover.style.left = event.target.offsetLeft.toString() + "px";
-                popover.style.display = "block";//add a class not display
+        var shadow = document.getElementById(constants.popoverID).createShadowRoot();
+        var inject2 = document.createElement("style");
+        httpGetAsynca("css/extension_popover.css",function (data2) {
+            inject2.innerHTML=data2;
+            shadow.appendChild(inject2);
+            inject2 = document.createElement("div");
+            inject2.innerHTML = data;
+            shadow.appendChild(inject2);
 
+            window.addEventListener("mouseover", function (event) {
+                if (event.target.tagName === "IMG" && event.target.id!=constants.popoverID) {
+                    var popover =  document.getElementById(constants.popoverID).shadowRoot.getElementById(constants.popoverID2);
+                    popover.style.top = event.target.offsetTop.toString() + "px";
+                    popover.style.left = event.target.offsetLeft.toString() + "px";
+                    popover.style.display = "block";//add a class not display
 
-                if (current_popover === undefined) {
-                    var modal = document.getElementById(constants.modal_button);
-                    modal.addEventListener("click", function () {
-                        console.log('done')
-                    })
+                    if (current_popover === undefined) {
+                        var modal = document.getElementById(constants.popoverID).shadowRoot.getElementById(constants.modal_button);
+                        modal.addEventListener("click", function () {
+                            console.log('done')
+                        })
+                    }
+                    current_popover = popover;
+
+                } else {
+                    if (current_popover != undefined && event.target.id!==constants.popoverID) {//null
+                        current_popover.style.display = "none";
+                        current_popover = null;
+                    }
                 }
-                current_popover = popover;
+            });
+        })
 
-            } else {
 
-                if (current_popover != undefined && findAncestor(event.target, constants.popoverMainClass) !== current_popover) {//null
-                    current_popover.style.display = "none";
-                    current_popover = null;
-                }
-            }
-        });
+
+
     });
 
 
