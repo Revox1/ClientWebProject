6d01dad59bdb8e69a85aeece8cef34a7e7133b72
Mutescu Alexandru Ciprian <revox1@users.noreply.github.com>
2017-11-22 20:12:14 +0200

    Adding a modal with basic tabs

--- js/content.js
+++ js/content.js
@@ -13,12 +13,12 @@ function httpGetAsynca(theUrl, callback) {
     xmlHttp.send(null);
 }
 
-function positionPopupOnPage( event,popover ) {
+function positionPopupOnPage(event, popover) {
     var scrollLeft = (window.pageXOffset !== undefined) ? window.pageXOffset : (document.documentElement || document.body.parentNode || document.body).scrollLeft;
     var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
     var vpWH = [];
     var vpW, vpH;
-    var intCoordX = event.clientX ;
+    var intCoordX = event.clientX;
     var intCoordY = event.clientY;
     var intXOffset = intCoordX;
     var intYOffset = intCoordY;
@@ -28,30 +28,47 @@ function positionPopupOnPage( event,popover ) {
     vpH = vpWH[1];
 
     popover.style.display = 'block';
-    if ( intCoordX > vpW/2 ) { intXOffset -= popover.offsetWidth; }
-    if ( intCoordY > vpH/2 ) { intYOffset -= popover.offsetHeight; }
+    if (intCoordX > vpW / 2) {
+        intXOffset -= popover.offsetWidth;
+    }
+    if (intCoordY > vpH / 2) {
+        intYOffset -= popover.offsetHeight;
+    }
 
 
-    popover.style.top = (intYOffset+scrollTop) + 'px';
-    popover.style.left = (intXOffset+scrollLeft) + 'px';
+    popover.style.top = (intYOffset + scrollTop) + 'px';
+    popover.style.left = (intXOffset + scrollLeft) + 'px';
 
-}	// end fn positionPopupOnPage
+}
+
+function openCity(evt, cityName) {
+    console.log(evt, cityName);
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementById(constants.popoverID).shadowRoot.querySelectorAll(".tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementById(constants.popoverID).shadowRoot.querySelectorAll(".tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(constants.popoverID).shadowRoot.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
 function getViewPortWidthHeight() {
 
     var viewPortWidth;
     var viewPortHeight;
 
-    if (typeof window.innerWidth !== undefined)
-    {
+    if (typeof window.innerWidth !== undefined) {
         viewPortWidth = window.innerWidth;
         viewPortHeight = window.innerHeight;
     }
 
     // IE6 in standards compliant mode (i.e. with a valid doctype as the
     // first line in the document)
-    else if (typeof document.documentElement !== undefined && typeof document.documentElement.clientWidth !== undefined && document.documentElement.clientWidth !==0)
-    {
+    else if (typeof document.documentElement !== undefined && typeof document.documentElement.clientWidth !== undefined && document.documentElement.clientWidth !== 0) {
         viewPortWidth = document.documentElement.clientWidth;
         viewPortHeight = document.documentElement.clientHeight;
     }
@@ -71,52 +88,56 @@ window.onload = function () {
 
         var shadow = document.getElementById(constants.popoverID).createShadowRoot();
         var inject2 = document.createElement("style");
-        httpGetAsynca("css/extension_popover.css",function (data2) {
-            inject2.innerHTML=data2;
+        httpGetAsynca("css/extension_popover.css", function (data2) {
+            inject2.innerHTML = data2;
             shadow.appendChild(inject2);
             inject2 = document.createElement("div");
             inject2.innerHTML = data;
             shadow.appendChild(inject2);
 
             window.addEventListener("mouseover", function (event) {
-                if (event.target.tagName === "IMG" && event.target.id!=constants.popoverID) {
+                if (event.target.tagName === "IMG" && event.target.id != constants.popoverID) {
 
-                    var popover =  document.getElementById(constants.popoverID).shadowRoot.getElementById(constants.popoverID2);
-                    positionPopupOnPage(event,popover);
+                    var popover = document.getElementById(constants.popoverID).shadowRoot.getElementById(constants.popoverID2);
+                    positionPopupOnPage(event, popover);
 
                     if (current_popover === undefined) {
                         var modal = document.getElementById(constants.popoverID).shadowRoot.getElementById(constants.modal_button);
+                        console.log(modal)
                         modal.addEventListener("click", function () {
-                            console.log('pornire modal')
-                            popover.style.display="none"
+                            popover.style.display = "none"
                         })
                     }
 
                     current_popover = popover;
 
                 } else {
-                    if (current_popover != undefined && event.target.id!==constants.popoverID) {//null
+                    if (current_popover != undefined && event.target.id !== constants.popoverID) {//null
                         current_popover.style.display = "none";
                         current_popover = null;
                     }
                 }
             });
-            window.addEventListener("scroll",function (e) {
-                if(current_popover!=undefined){
+            var buttons = document.getElementById(constants.popoverID).shadowRoot.querySelectorAll('.tablinks');
+            buttons.forEach(function (t) {
 
-                    current_popover.style.display="none"
+                t.addEventListener("click", function (event) {
+                    openCity(event, t.innerHTML);
+                });
+            });
+            window.addEventListener("scroll", function (e) {
+                if (current_popover != undefined) {
+                    current_popover.style.display = "none"
                 }
 
             });
-            window.addEventListener("resize",function (e) {
-                if(current_popover!=undefined){
+            window.addEventListener("resize", function (e) {
+                if (current_popover != undefined) {
 
-                    current_popover.style.display="none"
+                    current_popover.style.display = "none"
                 }
             });
         })
-
-
 
 
     });
